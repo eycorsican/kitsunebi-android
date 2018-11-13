adaa4b8f91795acafa68e1f632ae5c5b656ace84
eric <eric.y.corsican@gmail.com>
2018-11-13 08:44:33 +0800

    open sourcing android part

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Kitsunebi
 
-Not yet ready for opensourcing.
+A fully-featured V2Ray client for Android.
 
 ## 下载
 
@@ -14,9 +14,9 @@ Github Releases: https://github.com/eycorsican/Kitsunebi4Android/releases
 - 把配置文件复制粘贴至主界面后，点击连接按钮即可启动
 - 如果配置文件不正确或者出错，通常不会有任何错误提示
 - 配置文件可使用一个常见的 V2Ray 配置
-- 配置文件的 freedom outbound 需要使用 [`UseIP` 策略](https://www.v2ray.com/chapter_02/protocols/freedom.html#outboundconfigurationobject)
+- 配置文件的 freedom outbound 推荐使用 [`UseIP` 策略](https://www.v2ray.com/chapter_02/protocols/freedom.html#outboundconfigurationobject)
 - 配置文件不需要有 Inbound，app 使用了 `tun2socks` 作为 inbound，并已开启 [http,tls 流量嗅探](https://www.v2ray.com/chapter_02/01_overview.html#sniffingobject)
-- 如果 freedom outbound 正确配置了 `UseIP`，设备所有 DNS 请求均由 V2Ray 的 [DNS 服务器](https://www.v2ray.com/chapter_02/04_dns.html)来解析，正确设置了 DNS 服务器可以避免 DNS 污染以及 CDN 相关的 DNS 问题
+- 设备所有 DNS 请求均会由 V2Ray 的 [DNS 服务器](https://www.v2ray.com/chapter_02/04_dns.html) 来解析，正确设置了 DNS 服务器可以避免 DNS 污染以及 CDN 相关的 DNS 问题
 - 目前，非 VMess 协议的 outbound 不可以用域名来做服务器地址，必须要用 IP
 - 下面是一个示例配置：
 ```json
@@ -32,10 +32,7 @@ Github Releases: https://github.com/eycorsican/Kitsunebi4Android/releases
     "dns": {
         "clientIP": "115.239.211.92",  # 你的对外地址（或者随便找个同地区的 IP），用来提示 DNS 服务器返回合适的 IP
         "hosts": {
-            "localhost": "127.0.0.1",
-            "domain:lan": "127.0.0.1",
-            "domain:local": "127.0.0.1",
-            "domain:arpa": "127.0.0.1"
+            "localhost": "127.0.0.1"
         },
         "servers": [
             "8.8.8.8",
---
+++ app/src/main/java/fun/kitsunebi/kitsunebi4android/MainActivity.kt
@@ -0,0 +1,187 @@
+package `fun`.kitsunebi.kitsunebi4android
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import android.net.VpnService
+import android.content.Intent
+import android.widget.TextView
+import kotlinx.android.synthetic.main.content_main.*
+import org.json.JSONException
+import org.json.JSONObject
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.support.v4.app.NotificationCompat
+import android.content.BroadcastReceiver
+import android.content.IntentFilter
+
+
+class MainActivity : AppCompatActivity() {
+
+    var configString: String = ""
+    val mNotificationId = 1
+//    var mNotificationManager: NotificationManager? = null
+    var running = false
+
+    val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(contxt: Context?, intent: Intent?) {
+            when (intent?.action) {
+                "vpn_stopped" -> {
+                    running = false
+                    fab.setImageResource(android.R.drawable.ic_media_play)
+//                    mNotificationManager?.cancel(mNotificationId)
+                }
+                "vpn_started" -> {
+                    running = true
+                    fab.setImageResource(android.R.drawable.ic_media_pause)
+//                    startNotification()
+                }
+                "vpn_start_err" -> {
+                    running = false
+                    fab.setImageResource(android.R.drawable.ic_media_play)
+                    showAlert("Start VPN service failed")
+                }
+                "pong" -> {
+                    fab.setImageResource(android.R.drawable.ic_media_pause)
+                    running = true
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+//        mNotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        registerReceiver(broadcastReceiver, IntentFilter("vpn_stopped"))
+        registerReceiver(broadcastReceiver, IntentFilter("vpn_started"))
+        registerReceiver(broadcastReceiver, IntentFilter("vpn_start_err"))
+        registerReceiver(broadcastReceiver, IntentFilter("pong"))
+
+        sendBroadcast(Intent("ping"))
+
+        val sharedPref = this?.getSharedPreferences(
+                getString(R.string.config_preference), Context.MODE_PRIVATE) ?: return
+        configString = sharedPref.getString(getString(R.string.preference_config_key), getString(R.string.default_config))
+        configString?.let {
+            formatJsonString(it).let {
+                configView.setText(it, TextView.BufferType.EDITABLE)
+            }
+        }
+
+        fab.setOnClickListener { view ->
+            if (!running) {
+                fab.setImageResource(android.R.drawable.ic_media_ff)
+                configString = configView.text.toString()
+                val intent = VpnService.prepare(this)
+                if (intent != null) {
+                    startActivityForResult(intent, 1)
+                } else {
+                    onActivityResult(1, Activity.RESULT_OK, null);
+                }
+            } else {
+                sendBroadcast(Intent("stop_vpn"))
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sendBroadcast(Intent("ping"))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.format_btn -> {
+                val prettyText = formatJsonString(configView.text.toString())
+                prettyText?.let {
+                    configView.setText(it, TextView.BufferType.EDITABLE)
+                }
+                return true
+            }
+            R.id.save_btn -> {
+                val config = configView.text.toString()
+                val prettyText = formatJsonString(config)
+                if (prettyText == null) {
+                    showAlert("Invalid JSON")
+                    return true
+                }
+                val sharedPref = this?.getSharedPreferences(
+                        getString(R.string.config_preference), Context.MODE_PRIVATE) ?: return true
+                with (sharedPref.edit()) {
+                    putString(getString(R.string.preference_config_key), prettyText)
+                    commit()
+                }
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == RESULT_OK) {
+            val intent = Intent(this, KitsunebiVpnService::class.java)
+            intent.putExtra("config", configString)
+            startService(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(broadcastReceiver)
+    }
+
+    fun formatJsonString(json: String): String? {
+        return try {
+            JSONObject(json).toString(2)
+        } catch (e: JSONException) {
+            showAlert("Invalid JSON")
+            return null
+        }
+    }
+
+    fun showAlert(msg: String){
+        val dialog = AlertDialog.Builder(this).setTitle("Message").setMessage(msg)
+                .setPositiveButton("Ok", { dialog, i ->
+                })
+        dialog.show()
+    }
+
+//    private fun startNotification() {
+//        // Build Notification , setOngoing keeps the notification always in status bar
+//        val mBuilder = NotificationCompat.Builder(this, getString(R.string.notification_channel_id))
+//                .setContentTitle("Kitsunebi")
+//                .setContentText("Touch to open the app")
+//                .setSmallIcon(R.drawable.notification_icon_background)
+//                .setWhen(System.currentTimeMillis())
+//                .setOngoing(true)
+//
+//        // Create pending intent, mention the Activity which needs to be
+//        //triggered when user clicks on notification(StopScript.class in this case)
+//        val contentIntent = PendingIntent.getActivity(this, 0,
+//                Intent(this, MainActivity::class.java), PendingIntent.FLAG_UPDATE_CURRENT)
+//
+//        mBuilder.setContentIntent(contentIntent)
+//
+//        // Builds the notification and issues it.
+//        mNotificationManager?.notify(mNotificationId, mBuilder.build())
+//    }
+}
